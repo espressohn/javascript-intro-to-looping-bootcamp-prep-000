a51c3c87026f75ff97994888f4dcd338031ba27c
espressohn <espressohn@gmail.com>
2017-09-25 17:25:08 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,8 +1,9 @@
 function forLoop(array){
   for(let i=0; i<25; i++){
     array.push("I am ${i} strange loop${i===1 ? '' : 's'}.")
-    console.log(array[i])
+    //console.log(array[i])
   }
+  console.log(array)
   return array
 }
 
